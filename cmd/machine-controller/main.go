345bdf288906d0709c160238134e19f02ac590a1
Waleed Malik <ahmedwaleedmalik@gmail.com>
2023-04-03 15:22:46 +0500

    Validate node-container-runtime flag and allow only containerd
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- cmd/machine-controller/main.go
+++ cmd/machine-controller/main.go
@@ -170,7 +170,7 @@ func main() {
 	flag.StringVar(&nodeRegistryMirrors, "node-registry-mirrors", "", "Comma separated list of Docker image mirrors")
 	flag.StringVar(&nodePauseImage, "node-pause-image", "", "Image for the pause container including tag. If not set, the kubelet default will be used: https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/")
 	flag.String("node-kubelet-repository", "quay.io/kubermatic/kubelet", "[NO-OP] Repository for the kubelet container. Has no effects.")
-	flag.StringVar(&nodeContainerRuntime, "node-container-runtime", "docker", "container-runtime to deploy")
+	flag.StringVar(&nodeContainerRuntime, "node-container-runtime", "containerd", "container-runtime to deploy")
 	flag.StringVar(&nodeContainerdVersion, "node-containerd-version", "", "version of containerd to deploy")
 	flag.Var(&nodeContainerdRegistryMirrors, "node-containerd-registry-mirrors", "Configure registry mirrors endpoints. Can be used multiple times to specify multiple mirrors")
 	flag.StringVar(&caBundleFile, "ca-bundle", "", "path to a file containing all PEM-encoded CA certificates (will be used instead of the host's certificates if set)")
@@ -185,6 +185,10 @@ func main() {
 	flag.Parse()
 	kubeconfig = flag.Lookup("kubeconfig").Value.(flag.Getter).Get().(string)
 	masterURL = flag.Lookup("master").Value.(flag.Getter).Get().(string)
+
+	if nodeContainerRuntime != "containerd" {
+		klog.Fatalf("%s not supported; containerd is the only supported container runtime", nodeContainerRuntime)
+	}
 
 	clusterDNSIPs, err := parseClusterDNSIPs(clusterDNSIPs)
 	if err != nil {
